65c1d97d0d3c110eb46969a42a49f920deb78f96
Nishant Shaligram <nishant.shaligram@gmail.com>
2019-04-22 06:50:25 +0530

    Responsive Layout FIx
    Responsive Layout Fix in footer and all pages

--- footer.php
+++ footer.php
@@ -1,16 +1,18 @@
 </div>
 <footer class="site-footer">
     <div class="wrap">
-        <?php   if(is_active_sidebar('footer-1')): ?>
-        <div class="widget-area">
-        <?php   dynamic_sidebar('footer-1'); ?>
+        <div class="row">
+            <?php   if(is_active_sidebar('footer-1')): ?>
+            <div class="widget-area">
+            <?php   dynamic_sidebar('footer-1'); ?>
+            </div>
+            <?php   endif;  ?>
+            <?php   if(is_active_sidebar('footer-2')): ?>
+            <div class="widget-area">
+            <?php   dynamic_sidebar('footer-2'); ?>
+            </div>
+            <?php   endif;  ?>
         </div>
-        <?php   endif;  ?>
-        <?php   if(is_active_sidebar('footer-2')): ?>
-        <div class="widget-area">
-        <?php   dynamic_sidebar('footer-2'); ?>
-        </div>
-        <?php   endif;  ?>
     </div>
 </footer>
 <?php wp_footer(); ?>
